041371253509d08a6a921ca140a9b32e973728c8
Dongubak <91736442+Dongubak@users.noreply.github.com>
2024-12-03 21:48:36 +0900

    errorhandle: 유니코드 처리 오류
    errorhandle: 유니코드 처리 오류발생시
    에러 핸들링 추가

--- blog/blog-fronted/src/lib/api/food.js
+++ blog/blog-fronted/src/lib/api/food.js
@@ -1,8 +1,28 @@
 import client from './client';
 
-export const searchFood = (keyword) => {
-   console.log(keyword);
-  return client.get(`http://203.234.48.76:43306/recommend`, {
-    params: {word : keyword },
-  })
-}
+// 유니코드 디코딩 함수
+const unicodeToUtf8 = (str) => {
+  return str.replace(/\\u[\dA-F]{4}/gi, (match) => {
+    return String.fromCharCode(parseInt(match.replace("\\u", ""), 16));
+  });
+};
+
+// 응답 데이터 디코딩 처리 함수
+const parseUnicodeResponse = (data) => {
+  if (typeof data === 'string') {
+    // 유니코드 JSON 문자열을 파싱
+    return JSON.parse(unicodeToUtf8(data));
+  }
+  return data; // 데이터가 이미 JSON 객체라면 그대로 반환
+};
+
+// 음식 검색 API 호출
+export const searchFood = async (keyword) => {
+  console.log(keyword);
+  const response = await client.get(`http://203.234.48.76:43306/recommend`, {
+    params: { word: keyword },
+  });
+
+  // 유니코드 데이터 디코딩 후 반환
+  return parseUnicodeResponse(response.data);
+};
--- blog/blog-fronted/src/modules/food.js
+++ blog/blog-fronted/src/modules/food.js
@@ -3,46 +3,51 @@ import createRequestSaga, {
   createRequestActionTypes,
 } from '../lib/createRequestSaga';
 import * as foodAPI from '../lib/api/food';
-import { takeLatest } from 'redux-saga/effects';
+import { takeLatest, call, put } from 'redux-saga/effects';
 
 const [
-   SERACH_FOOD,
-   SERACH_FOOD_SUCCESS,
-   SEARCH_FOOD_FAILURE
- ] = createRequestActionTypes('food/SEARCH_FOOD');
+  SEARCH_FOOD,
+  SEARCH_FOOD_SUCCESS,
+  SEARCH_FOOD_FAILURE,
+] = createRequestActionTypes('food/SEARCH_FOOD');
 
-export const searchFood = createAction(
-   SERACH_FOOD,
-   (keyword) => keyword,
-);
+export const searchFood = createAction(SEARCH_FOOD, (keyword) => keyword);
 
-
-const searchFoodSaga = createRequestSaga(SERACH_FOOD, foodAPI.searchFood);
+// Saga에서 응답 처리
+function* searchFoodSaga(action) {
+  try {
+    const response = yield call(foodAPI.searchFood, action.payload);
+    yield put({ type: SEARCH_FOOD_SUCCESS, payload: response });
+  } catch (error) {
+    yield put({ type: SEARCH_FOOD_FAILURE, payload: error.message });
+  }
+}
 
 export function* FoodSaga() {
-   yield takeLatest(SERACH_FOOD, searchFoodSaga);
+  yield takeLatest(SEARCH_FOOD, searchFoodSaga);
 }
 
 const initialState = {
-   data: '',
-   error: false,
-   errorMSG: '',
-}
+  data: '',
+  error: false,
+  errorMSG: '',
+};
 
 const food = handleActions(
-   {
-     [SERACH_FOOD_SUCCESS]: (state, { payload: response }) => ({
-         ...state,
-         error: false,
-       data: response
-     }),
+  {
+    [SEARCH_FOOD_SUCCESS]: (state, { payload: response }) => ({
+      ...state,
+      error: false,
+      data: response,
+    }),
 
-     [SEARCH_FOOD_FAILURE]: (state, { payload: error }) => ({
+    [SEARCH_FOOD_FAILURE]: (state, { payload: error }) => ({
+      ...state,
       error: true,
-      errorMSG: error
-     })
-   },
-   initialState,
- );
+      errorMSG: error,
+    }),
+  },
+  initialState,
+);
 
- export default food;
+export default food;
